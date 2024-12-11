13aca9048652bad1fb51de7c576b3339d8af7a1e
ihebbn <mohamediheb.benslama@esprit.tn>
2024-12-11 21:30:10 +0100

    next config update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 module.exports = {
   reactStrictMode: true,
-}
+  output: 'export', // Ensures static export for GitHub Pages
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    unoptimized: true, // Required for Next.js export if you're using <Image>
+  },
+};
