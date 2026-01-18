0f59132c538cfa9257387c2323c2f8f7c385bc03
Mohamed Iheb Ben Slama <mohamediheb.benslama@gmail.com>
2026-01-18 13:08:13 +0000

    Configure GA4 env variable

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,41 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
-import { AnimatePresence } from 'framer-motion'
-import theme from 'config/theme'
-import FavIconProvider from 'components/Misc/FavIconProvider'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { AnimatePresence } from "framer-motion";
+import theme from "config/theme";
+import FavIconProvider from "components/Misc/FavIconProvider";
+import Script from "next/script";
 
 function KLSite({ Component, pageProps }: AppProps): JSX.Element {
+  const GA_ID = process.env.NEXT_PUBLIC_ANALYTICS_ID;
+
   return (
     <AnimatePresence exitBeforeEnter>
       <ChakraProvider theme={theme}>
         <FavIconProvider>
+          {/* Google Analytics (GA4) */}
+          {GA_ID && (
+            <>
+              <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+                strategy="afterInteractive"
+              />
+              <Script id="ga4" strategy="afterInteractive">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', { send_page_view: true });
+                `}
+              </Script>
+            </>
+          )}
+
           <Component {...pageProps} />
         </FavIconProvider>
       </ChakraProvider>
     </AnimatePresence>
-  )
+  );
 }
-export default KLSite
+
+export default KLSite;
